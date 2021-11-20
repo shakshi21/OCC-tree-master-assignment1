57e326642536c3ba7f2ac1aa49d8d37776c4f853
shakshi21 <93274543+shakshi21@users.noreply.github.com>
2021-11-20 19:27:37 +0530

    Add files via upload

---
+++ Assignment13/app.js
@@ -0,0 +1,16 @@
+function add(){
+    const title = document.getElementById("title").value;
+    const desc = document.getElementById("desc").value;
+    let li = document.createElement("li");
+    let uList = document.getElementById("uList")
+
+    const Todo = {
+        title:title,
+        desc:desc,
+    }
+    console.log(Todo);
+    li.innerHTML = JSON.stringify(Todo);
+    uList.appendChild(li)
+
+    
+}
